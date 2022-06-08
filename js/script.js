5801d05512adb5b0a3509c1191c25736db9dba84
gitAji <89026824+gitAji@users.noreply.github.com>
2022-06-08 15:39:53 +0200

    mq

--- js/script.js
+++ js/script.js
@@ -1,66 +1,43 @@
+const x = document.querySelector(".theme-switch");
+const y = document.querySelector("body");
 
-const x=document.querySelector(".theme-switch");
-const y=document.querySelector("body");
-
-x.addEventListener("click",function(){
+x.addEventListener("click", function () {
     y.classList.toggle("dark-mode");
-}
-)
+})
 
 // hamburger menu 
-const hamburger=document.querySelector(".hamburger");
-const menu=document.querySelector("nav");
-const close=document.querySelector(".close");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("nav");
+const close = document.querySelector(".close");
 
-hamburger.addEventListener("click",function(){
+hamburger.addEventListener("click", function () {
     console.log("clicked");
-  menu.style.display="flex"; 
+    menu.style.display = "flex";
 })
-close.addEventListener("click",function(){
-    menu.style.display="none";
-}
-)
+close.addEventListener("click", function () {
+    menu.style.display = "none";
+})
 
-const leftArrow=document.querySelector(".left-arrow");
-const rightArrow=document.querySelector(".right-arrow");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
 
-leftArrow.addEventListener("click",function(){
+leftArrow.addEventListener("click", function () {
     console.log("clicked");
- 
+
 });
 
 // back to top
-const btn=document.querySelector(".back-top");
+const btn = document.querySelector(".back-top");
 
 
-window.addEventListener("scroll",function(){
-    if(window.scrollY>300){
-        btn.style.display="block";
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
     }
-    else{
-        btn.style.display="none";
-    }
-}
-)
+})
 
-btn.addEventListener("click",function(){
-    window.scrollTo(0,0);
-}
-)
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
+btn.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+})
