fd26af7aca56ccfa0bc8dfbf343327e3cfab6a02
gitAji <89026824+gitAji@users.noreply.github.com>
2022-06-09 12:18:08 +0200

    All most there

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const btn = document.querySelector(".back-top");
 
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 300) {
+    if (window.scrollY > 500) {
         btn.style.display = "block";
     } else {
         btn.style.display = "none";
@@ -41,3 +41,54 @@ window.addEventListener("scroll", function () {
 btn.addEventListener("click", function () {
     window.scrollTo(0, 0);
 })
+
+// contact form validation
+
+const submit = document.querySelector(".contact");
+const fullName = document.querySelector("#fullName");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const nameError = document.querySelector("#nameError");
+const emailError = document.querySelector("#emailError");
+const messageError = document.querySelector("#messageError");
+const success = document.querySelector(".success-message");
+
+
+function validateForm(e) {
+    console.log("clicked");
+    e.preventDefault();
+
+    if (fullName.value === "") {
+        nameError.style.display = "block";
+        nameError.style.color = "#f94306bc";
+        nameError.innerHTML = "Please enter your name";
+    } else {
+        nameError.style.display = "none";
+    }
+    if (checkEmail(email.value) === false) {
+        emailError.style.display = "block";
+        emailError.style.color = "#f94306bc";
+        emailError.innerHTML = "Please enter your valid email";
+    } else {
+        emailError.style.display = "none";
+    }
+    if (message.value === "") {
+        messageError.style.display = "block";
+        messageError.style.color = "#f94306bc";
+        messageError.innerHTML = "Please enter your message";
+    } else {
+        messageError.style.display = "none";
+    }
+    if (fullName.value !== "" && checkEmail(email.value) === true && message.value !== "") {
+        success.style.display = "block";
+        success.style.color = "green";
+        success.innerHTML = "Your message has been sent. Thank you!";
+    }
+}
+
+
+function checkEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+submit.addEventListener("click", validateForm, checkEmail);
